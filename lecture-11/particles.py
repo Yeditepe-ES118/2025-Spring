f51d35773265247642664e91ebb808d744e991c7
utblr <ufuktan@utblr.xyz>
2025-05-05 11:45:34 +0300

    change the computation, and add information

--- lecture-11/particles.py
+++ lecture-11/particles.py
@@ -1,3 +1,12 @@
+# before you run this code,
+# firstly, open git bash and run:
+# git clone https://github.com/Yeditepe-ES118/2025-Spring.git
+# then:
+# cd 2025-Spring/lecture-11
+# then you can find this code and all the .txt files used
+# in this script
+# and lastly, run this script
+
 import numpy as np
 
 positions = np.loadtxt("positions.txt", delimiter=",") # m
@@ -6,19 +15,13 @@ masses = np.loadtxt("masses.txt", delimiter=",") # kg
 
 g = np.array([0, -9.81]) # kg/m-s2
 
-# change the shape for mass which is
-# going to be used in the element-wise multiplication
-# in the for loop; mass * g -> dimensions (100,1) * (100, 2) 
-masses = np.reshape(masses, (masses.size, 1))
-# where masses.size is 100 in this case
-
 # time increment between the consecutive time steps
 dt = 0.01 # s
 
 # simulate forces, velocities, and positions for 100 time steps
 for step in range(100):
     # calculate force (just gravity in this simple case)
-    forces = masses * g  # N
+    forces = np.array([[g[0] * masses, g[1] * masses]]) # N
 
     # update velocities using dv = g*dt
     velocities += g * dt # m/s
--- lecture-11/particles_plot.py
+++ lecture-11/particles_plot.py
@@ -1,7 +1,17 @@
+# before you run this code,
+# firstly, open git bash and run:
+# git clone https://github.com/Yeditepe-ES118/2025-Spring.git
+# then:
+# cd 2025-Spring/lecture-11
+# then you can find this code and all the .txt files used
+# in this script
+# and lastly, run this script
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 positions = np.loadtxt("positions.txt", delimiter=",") # m
+
 velocities = np.loadtxt("velocities.txt", delimiter=",") # m
 masses = np.loadtxt("masses.txt", delimiter=",") # kg
 
@@ -10,19 +20,13 @@ g = np.array([0, -9.81]) # kg/m-s2
 fig, ax = plt.subplots()
 ax.plot(positions[:,0], positions[:,1], "ro", label="t = 0 s")
 
-# change the shape for mass which is
-# going to be used in the element-wise multiplication
-# in the for loop; mass * g -> dimensions (100,1) * (100, 2) 
-masses = np.reshape(masses, (masses.size, 1))
-# where masses.size is 100 in this case
-
 # time increment between the consecutive time steps
 dt = 0.01 # s
 
 # simulate forces, velocities, and positions for 100 time steps
 for step in range(100):
     # calculate force (just gravity in this simple case)
-    forces = masses * g  # N
+    forces = np.array([[g[0] * masses, g[1] * masses]]) # N
 
     # update velocities using dv = g*dt
     velocities += g * dt # m/s
